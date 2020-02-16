270b3c5286c1d89a0458bcfccd046d9baa73e219
vzhilin <zhilin.vladimir.nic@gmail.com>
2020-02-16 14:04:47 +0300

    remove extra code

--- src/main/java/me/vzhilin/auth/CredentialsProvider.java
+++
@@ -1,6 +0,0 @@
-package me.vzhilin.auth;
-
-@FunctionalInterface
-interface CredentialsProvider {
-    Credentials getCredentials(String host, int port, String uri, String realm);
-}
--- src/main/java/me/vzhilin/auth/DigestAuthenticatorProvider.java
+++
@@ -1,6 +0,0 @@
-package me.vzhilin.auth;
-
-@FunctionalInterface
-public interface DigestAuthenticatorProvider {
-    DigestAuthenticator authenticatorFor(String host, int port, String resource);
-}
