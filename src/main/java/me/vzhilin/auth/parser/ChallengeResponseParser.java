4a808ecc1bf138ad4ea9a8d5ff514a74ca1ebe3a
Jeremy Rumpf <jeremy.rumpf@laufservices.com>
2021-04-20 07:01:03 -0400

    Convert tabs to whitespace.

--- src/main/java/me/vzhilin/auth/parser/ChallengeResponseParser.java
+++ src/main/java/me/vzhilin/auth/parser/ChallengeResponseParser.java
@@ -139,8 +139,8 @@ public final class ChallengeResponseParser extends CommonAuthHeadersParser {
         } while (hasNext());
 
         if (challenge.getAlgorithm() == null) {
-        	challenge.addAlgorithm(DigestAlgorithm.MD5);
-	}
+            challenge.addAlgorithm(DigestAlgorithm.MD5);
+        }
 
         return challenge;
     }
